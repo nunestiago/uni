da0e74e2b79fc35d78892183ecea18ba08cf1a94
Tiago Nunes <ntiagon@gmail.com>
2023-05-07 15:08:30 -0300

    a

---
+++ ParadigmasDeLinguagensPython/tema4.py
@@ -0,0 +1,9 @@
+test = "Hello World"
+print(test)
+nome = input("Lalala input()")
+print(nome)
+
+peso = eval(input("Colocar peso"))
+altura = eval(input("Colocar altura"))
+imc = peso/(altura**2)
+print(imc)
